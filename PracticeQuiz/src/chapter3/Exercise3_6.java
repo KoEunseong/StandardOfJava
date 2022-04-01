f289c2e758e32d0b81891afac31705049d15782b
Eunseong Ko <dmsdl261@naver.com>
2022-04-01 17:48:19 +0900

    IU-2021.3.3 <82109@DESKTOP-P6HCNDC Overwrite remote https://github.com/KoEunseong/StandardOfJava to local

--- PracticeQuiz/src/chapter3/Exercise3_6.java
+++
@@ -1,8 +0,0 @@
-package chapter3;
-
-public class Exercise3_6 {
-	public static void main(String[] args) {
-		int num = 24;
-		System.out.println(num % 10 == 0? num: (num+ (10 - num % 10)));
-	}
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Standard Of Java 3rd Edition
-자바의 정석 3rd 버전 개인 코드 공부와 정리
